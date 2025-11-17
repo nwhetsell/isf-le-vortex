95f1b4199a0cb06c950da5a93716603c7e5b59e8
Nate Whetsell <nathan.whetsell@gmail.com>
2025-11-17 05:01:54 -0500

    Reformat

--- Torus Dystopia.fs
+++ Torus Dystopia.fs
@@ -73,87 +73,142 @@
 #define time iTime
 
 // raymarching toolbox
-float rng (vec2 seed) { return fract(sin(dot(seed*.1684,vec2(54.649,321.547)))*450315.); }
-mat2 rot (float a) { float c=cos(a),s=sin(a); return mat2(c,-s,s,c); }
-float sdSphere (vec3 p, float r) { return length(p)-r; }
-float sdCylinder (vec2 p, float r) { return length(p)-r; }
-float sdDisk (vec3 p, vec3 s) { return max(max(length(p.xz)-s.x, s.y), abs(p.y)-s.z); }
-float sdIso(vec3 p, float r) { return max(0.,dot(p,normalize(sign(p))))-r; }
-float sdBox( vec3 p, vec3 b ) { vec3 d = abs(p) - b; return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0)); }
-float sdTorus( vec3 p, vec2 t ) { vec2 q = vec2(length(p.xz)-t.x,p.y); return length(q)-t.y; }
-float amod (inout vec2 p, float count) { float an = TAU/count; float a = atan(p.y,p.x)+an/2.; float c = floor(a/an); c = mix(c,abs(c),step(count*.5,abs(c))); a = mod(a,an)-an/2.; p.xy = vec2(cos(a),sin(a))*length(p); return c; }
-float amodIndex (vec2 p, float count) { float an = TAU/count; float a = atan(p.y,p.x)+an/2.; float c = floor(a/an); c = mix(c,abs(c),step(count*.5,abs(c))); return c; }
-float repeat (float v, float c) { return mod(v,c)-c/2.; }
-vec2 repeat (vec2 v, vec2 c) { return mod(v,c)-c/2.; }
-vec3 repeat (vec3 v, float c) { return mod(v,c)-c/2.; }
-float smoo (float a, float b, float r) { return clamp(.5+.5*(b-a)/r, 0., 1.); }
-float smin (float a, float b, float r) { float h = smoo(a,b,r); return mix(b,a,h)-r*h*(1.-h); }
-float smax (float a, float b, float r) { float h = smoo(a,b,r); return mix(a,b,h)+r*h*(1.-h); }
-vec2 displaceLoop (vec2 p, float r) { return vec2(length(p.xy)-r, atan(p.y,p.x)); }
-float map (vec3);
-float getShadow (vec3 pos, vec3 at, float k) {
+float rng(vec2 seed) {
+    return fract(sin(dot(seed * 0.1684, vec2(54.649, 321.547))) * 450315.);
+}
+
+mat2 rot(float a) {
+    float c = cos(a);
+    float s = sin(a);
+    return mat2(c, -s, s, c);
+}
+
+float sdCylinder(vec2 p, float r) {
+    return length(p) - r;
+}
+
+float sdBox(vec3 p, vec3 b) {
+    vec3 d = abs(p) - b;
+    return min(max(d.x, max(d.y, d.z)), 0.) + length(max(d, 0.));
+}
+
+float sdTorus(vec3 p, vec2 t) {
+    vec2 q = vec2(length(p.xz) - t.x, p.y);
+    return length(q) - t.y;
+}
+
+float amod(inout vec2 p, float count) {
+    float an = TAU / count;
+    float a = atan(p.y, p.x) + an / 2.;
+    float c = floor(a / an);
+    c = mix(c, abs(c), step(count * 0.5, abs(c)));
+    a = mod(a, an) - an / 2.;
+    p.xy = vec2(cos(a), sin(a)) * length(p);
+    return c;
+}
+
+float amodIndex(vec2 p, float count) {
+    float an = TAU / count;
+    float a = atan(p.y, p.x) + an / 2.;
+    float c = floor(a / an);
+    c = mix(c, abs(c), step(count * 0.5, abs(c)));
+    return c;
+}
+
+float repeat(float v, float c) { return mod(v, c) - c / 2.; }
+vec2  repeat(vec2 v,  vec2 c)  { return mod(v, c) - c / 2.; }
+vec3  repeat(vec3 v,  float c) { return mod(v, c) - c / 2.; }
+
+float smoo(float a, float b, float r) { return clamp(0.5 + 0.5 * (b - a) / r, 0., 1.); }
+float smin(float a, float b, float r) {
+    float h = smoo(a, b, r);
+    return mix(b, a, h) - r * h * (1. - h);
+}
+float smax(float a, float b, float r) {
+    float h = smoo(a, b, r);
+    return mix(a, b ,h) + r * h * (1. - h);
+}
+
+vec2 displaceLoop(vec2 p, float r) {
+    return vec2(length(p.xy) - r, atan(p.y, p.x));
+}
+
+float map(vec3);
+float getShadow(vec3 pos, vec3 at, float k) {
     vec3 dir = normalize(at - pos);
     float maxt = length(at - pos);
     float f = 01.;
-    float t = VOLUME*50.;
-    for (float i = 0.; i <= 1.; i += 1./15.) {
+    float t = VOLUME * 50.;
+    for (float i = 0.; i <= 1.; i += 1. / 15.) {
         float dist = map(pos + dir * t);
-        if (dist < VOLUME) return 0.;
+        if (dist < VOLUME) {
+            return 0.;
+        }
         f = min(f, k * dist / t);
         t += dist;
-        if (t >= maxt) break;
+        if (t >= maxt) {
+            break;
+        }
     }
     return f;
 }
-vec3 getNormal (vec3 p) { vec2 e = vec2(.01,0); return normalize(vec3(map(p+e.xyy)-map(p-e.xyy),map(p+e.yxy)-map(p-e.yxy),map(p+e.yyx)-map(p-e.yyx))); }
 
-void camera (inout vec3 p) {
-    p.xz *= rot(PI/8.);
-    p.yz *= rot(PI/6.);
+vec3 getNormal(vec3 p) {
+    vec2 e = vec2(0.01, 0);
+    return normalize(vec3(
+        map(p + e.xyy) - map(p - e.xyy),
+        map(p + e.yxy) - map(p - e.yxy),
+        map(p + e.yyx) - map(p - e.yyx)
+    ));
 }
 
-float windowCross (vec3 pos, vec4 size, float salt) {
+void camera(inout vec3 p) {
+    p.xz *= rot(PI / 8.);
+    p.yz *= rot(PI / 6.);
+}
+
+float windowCross(vec3 pos, vec4 size, float salt) {
     vec3 p = pos;
-    float sx = size.x * (.6+salt*.4);
-    float sy = size.y * (.3+salt*.7);
-    vec2 sxy = vec2(sx,sy);
-    p.xy = repeat(p.xy+sxy/2., sxy);
-    float scene = sdBox(p, size.zyw*2.);
-    scene = min(scene, sdBox(p, size.xzw*2.));
+    float sx = size.x * (0.6 + salt * 0.4);
+    float sy = size.y * (0.3 + salt * 0.7);
+    vec2 sxy = vec2(sx, sy);
+    p.xy = repeat(p.xy + sxy / 2., sxy);
+    float scene = sdBox(p, size.zyw * 2.);
+    scene = min(scene, sdBox(p, size.xzw * 2.));
     scene = max(scene, sdBox(pos, size.xyw));
     return scene;
 }
 
-float window (vec3 pos, vec2 dimension, float salt) {
-    float thinn = .008;
-    float depth = .04;
-    float depthCadre = .06;
-    float padding = .08;
-    float scene = windowCross(pos, vec4(dimension,thinn,depth), salt);
+float window(vec3 pos, vec2 dimension, float salt) {
+    float thinn = 0.008;
+    float depth = 0.04;
+    float depthCadre = 0.006;
+    float padding = 0.08;
+    float scene = windowCross(pos, vec4(dimension, thinn, depth), salt);
     float cadre = sdBox(pos, vec3(dimension, depthCadre));
-    cadre = max(cadre, -sdBox(pos, vec3(dimension - padding, depthCadre*2.)));
+    cadre = max(cadre, -sdBox(pos, vec3(dimension - padding, depthCadre * 2.)));
     scene = min(scene, cadre);
     return scene;
 }
 
-float boxes (vec3 pos, float salt) {
+float boxes(vec3 pos, float salt) {
     vec3 p = pos;
-    float ry = cell * .43*(.3+salt);
-    float rz = cell * .2*(.5+salt);
-    float salty = rng(vec2(floor(pos.y/ry), floor(pos.z/rz)));
+    float ry = cell * 0.43 * (0.3 + salt);
+    float rz = cell * 0.2 * (0.5 + salt);
+    float salty = rng(vec2(floor(pos.y / ry), floor(pos.z / rz)));
     pos.y = repeat(pos.y, ry);
     pos.z = repeat(pos.z, rz);
-    float scene = sdBox(pos, vec3(.1+.8*salt+salty,.1+.2*salt,.1+.2*salty));
-    scene = max(scene, sdBox(p, vec3(cell*.2)));
+    float scene = sdBox(pos, vec3(0.1 + 0.8 * salt + salty, 0.1 + 0.2 * salt, 0.1 + 0.2 * salty));
+    scene = max(scene, sdBox(p, vec3(cell * 0.2)));
     return scene;
 }
 
-float map (vec3 pos) {
-    vec3 camOffset = vec3(-4,0,0.);
+float map(vec3 pos) {
+    vec3 camOffset = vec3(-4, 0, 0);
 
     float scene = 1000.;
     vec3 p = pos + camOffset;
-    float segments = PI*radius;
+    float segments = PI * radius;
     float indexX, indexY, salt;
     vec2 seed;
 
@@ -164,7 +219,7 @@ float map (vec3 pos) {
     pDonut.xz = displaceLoop(pDonut.xz, donut);
     pDonut.z *= donut;
     pDonut.xzy = pDonut.xyz;
-    pDonut.xz *= rot(time*.05*speed);
+    pDonut.xz *= rot(time * 0.05 * speed);
 
     // ground
     p = pDonut;
@@ -173,34 +228,34 @@ float map (vec3 pos) {
     // walls
     p = pDonut;
     float py = p.y + time * speed;
-    indexY = floor(py / (cell+thin));
-    p.y = repeat(py, cell+thin);
-    scene = min(scene, max(abs(p.y)-thin, sdCylinder(p.xz, radius)));
+    indexY = floor(py / (cell + thin));
+    p.y = repeat(py, cell + thin);
+    scene = min(scene, max(abs(p.y) - thin, sdCylinder(p.xz, radius)));
     amod(p.xz, segments);
     p.x -= radius;
-    scene = min(scene, max(abs(p.z)-thin, p.x));
+    scene = min(scene, max(abs(p.z) - thin, p.x));
 
-    // horizontal windot
+    // horizontal window
     p = pDonut;
-    p.xz *= rot(PI/segments);
+    p.xz *= rot(PI / segments);
     py = p.y + time * speed;
-    indexY = floor(py / (cell+thin));
-    p.y = repeat(py, cell+thin);
+    indexY = floor(py / (cell + thin));
+    p.y = repeat(py, cell + thin);
     indexX = amodIndex(p.xz, segments);
     amod(p.xz, segments);
     seed = vec2(indexX, indexY);
     salt = rng(seed);
     p.x -= radius;
-    vec2 dimension = vec2(.75,.5);
+    vec2 dimension = vec2(0.75, 0.5);
     p.x +=  dimension.x * 1.5;
-    scene = max(scene, -sdBox(p, vec3(dimension.x, .1, dimension.y)));
+    scene = max(scene, -sdBox(p, vec3(dimension.x, 0.1, dimension.y)));
     scene = min(scene, window(p.xzy, dimension, salt));
 
     // vertical window
     p = pDonut;
-    py = p.y + cell/2. + time * speed;
-    indexY = floor(py / (cell+thin));
-    p.y = repeat(py, cell+thin);
+    py = p.y + cell / 2. + time * speed;
+    indexY = floor(py / (cell + thin));
+    p.y = repeat(py, cell + thin);
     indexX = amodIndex(p.xz, segments);
     amod(p.xz, segments);
     seed = vec2(indexX, indexY);
@@ -208,15 +263,15 @@ float map (vec3 pos) {
     p.x -= radius;
     dimension.y = 1.5;
     p.x +=  dimension.x * 1.25;
-    scene = max(scene, -sdBox(p, vec3(dimension, .1)));
+    scene = max(scene, -sdBox(p, vec3(dimension, 0.1)));
     scene = min(scene, window(p, dimension, salt));
 
     // elements
     p = pDonut;
-    p.xz *= rot(PI/segments);
-    py = p.y + cell/2. + time * speed;
-    indexY = floor(py / (cell+thin));
-    p.y = repeat(py, cell+thin);
+    p.xz *= rot(PI / segments);
+    py = p.y + cell / 2. + time * speed;
+    indexY = floor(py / (cell + thin));
+    p.y = repeat(py, cell + thin);
     indexX = amodIndex(p.xz, segments);
     amod(p.xz, segments);
     seed = vec2(indexX, indexY);
@@ -227,29 +282,30 @@ float map (vec3 pos) {
     return scene;
 }
 
-void main() {
-    vec2 uv = (coord.xy-.5*iResolution.xy)/iResolution.y;
-    vec3 eye = vec3(0,0,-20);
+void main()
+{
+    vec2 uv = (coord.xy - 0.5 * iResolution.xy) / iResolution.y;
+    vec3 eye = vec3(0, 0, -20);
     vec3 ray = normalize(vec3(uv, 1.3));
     camera(eye);
     camera(ray);
-    float dither = rng(uv+fract(time));
+    float dither = rng(uv + fract(time));
     vec3 pos = eye;
     float shade = 0.;
-    for (float i = 0.; i <= 1.; i += 1./STEPS) {
+    for (float i = 0.; i <= 1.; i += 1. / STEPS) {
         float dist = map(pos);
         if (dist < VOLUME) {
-            shade = 1.-i;
+            shade = 1. - i;
             break;
         }
-        dist *= .5 + .1 * dither;
+        dist *= 0.5 + 0.1 * dither;
         pos += ray * dist;
     }
-    vec3 light = vec3(40.,100.,-10.);
+    vec3 light = vec3(40, 100, -10);
     float shadow = getShadow(pos, light, 4.);
     color = vec4(1);
     color *= shade;
     color *= shadow;
-    color = smoothstep(.0, .5, color);
+    color = smoothstep(0., 0.5, color);
     color.rgb = sqrt(color.rgb);
 }
